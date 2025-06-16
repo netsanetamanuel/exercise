cd88bdf3bb9de9cb2619541c653662e716b6f6e3
netsanetamanuel <abrehotamanuell@gmail.com>
2025-06-16 07:33:28 -0900

    initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// 1. check two numb and return if one of the number is 
+// 100 or if the sum of two is 100
+// return condition ? iftrue : iffalse
+// const checkNum = (a,b) =>
+//      a== 100 ||  b == 100  || (a+b) === 100;
+
+// console.log(checkNum(3,4));
+// console.log(checkNum(80,20))  
+
+// 2. program to find extension of file name 
+
+
+// const getFilext = (str) => {
+//     //str.slice(str.lastIndexOf('.'));
+//     const newStr = str.split('.')
+//     return newStr.pop();
+// }
+
+
+// console.log(getFilext('index.html'))
+
+// str = 'filename.txt.py'
+
+// console.log(str.split('.').join(' ,'))
+ 
+// 3. replace with succeser aplhabet (abcd)=>(bcde)
+
+// const replace = (str) => 
+//     str.split('').map(char => String.fromCharCode(char.charCodeAt()+1)).join( )
+
+// console.log(replace('split'))
+ 
+
+// 4. get date 
+
+//  const currentDate = new Date;
+// const days = currentDate.getDay();
+// const months = currentDate.getMonth();
+// const year = currentDate.getFullYear();
+
+// console.log(days,months,year)
+  
+
+// and !new to string if it begin with !new return og
+
+// const str = (str) => 
+//     str.startsWith('!new') ? str : '!new' +str ;
+//      str.indexOf('!new') === 0 ? str : 
+
+// console.log(str('star'))
+
+
+
+
+
+
+
+   
+
+
+
